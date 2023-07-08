7e69391a397f0f5d309b0b49147fac4a6fee79f2
Mario Ruiz <marioruiz434@gmail.com>
2023-07-08 15:07:26 -0400

    full code 1

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter);
+app.use("/users", usersRouter);
 
 // Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
